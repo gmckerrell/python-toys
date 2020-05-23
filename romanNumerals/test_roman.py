034868f7ec853074b6512cc6fd7fad3582ce5f08
Graeme McKerrell <31652935+gmckerrell@users.noreply.github.com>
2020-05-23 12:38:40 +0100

    Update test_roman.py

--- romanNumerals/test_roman.py
+++ romanNumerals/test_roman.py
@@ -1,3 +1,29 @@
+
+@pytest.mark.parametrize(
+    "numeral, expected", 
+    [
+    ('II',    2),
+    ('III',   3),
+    ('IV',    4),
+    ('V',     5),
+    ('IX',    9),
+    ('X',    10),
+    ('XX',   20),
+    ('XXX',  30),
+    ('XL',   40)
+    ('L',    50),
+    ('XC',   90),
+    ('C',   100),
+    ('CC',  200),
+    ('CCC', 300)
+    ('CD',  400),
+    ('D',   500),
+    ('CM',  900),
+    ('M',  1000),
+    ]
+)
+
+
 def _checkGoodValue(value, expected):
     assert asDecimal(value) == expected
         #------------------------------------
@@ -10,24 +36,7 @@ def _checkGoodValue(value, expected):
         
 def test_single_digits():
     assert asDecimal('I') == 1
-    ('II',    2)
-    ('III',   3)
-    ('IV',    4)
-    ('V',     5)
-    ('IX',    9)
-    ('X',    10)
-    ('XX',   20)
-    ('XXX',  30)
-    ('XL',   40)
-    ('L',    50)
-    ('XC',   90)
-    ('C',   100)
-    ('CC',  200)
-    ('CCC', 300)
-    ('CD',  400)
-    ('D',   500)
-    ('CM',  900)
-    ('M',  1000)
+    
 
 def test_multiple_digits(self):
             self._checkGoodValue('XI',           11)
