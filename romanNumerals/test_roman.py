009ea01de15d39833e933da3fa4962cab8c7765f
Graeme McKerrell <31652935+gmckerrell@users.noreply.github.com>
2020-05-24 08:17:56 +0100

    Update test_roman.py

--- romanNumerals/test_roman.py
+++ romanNumerals/test_roman.py
@@ -4,27 +4,28 @@ import roman
 @pytest.mark.parametrize(
     "single_numeral, expected", 
     [
-    ('I',     1),
-    ('II',    2),
-    ('III',   3),
-    ('IV',    4),
-    ('V',     5),
-    ('IX',    9),
-    ('X',    10),
-    ('XX',   20),
-    ('XXX',  30),
-    ('XL',   40),
-    ('L',    50),
-    ('XC',   90),
-    ('C',   100),
-    ('CC',  200),
-    ('CCC', 300),
-    ('CD',  400),
-    ('D',   500),
-    ('CM',  900),
-    ('M',  1000),
+        ('I',     1),
+        ('II',    2),
+        ('III',   3),
+        ('IV',    4),
+        ('V',     5),
+        ('IX',    9),
+        ('X',    10),
+        ('XX',   20),
+        ('XXX',  30),
+        ('XL',   40),
+        ('L',    50),
+        ('XC',   90),
+        ('C',   100),
+        ('CC',  200),
+        ('CCC', 300),
+        ('CD',  400),
+        ('D',   500),
+        ('CM',  900),
+        ('M',  1000),
     ]
 )
+
 
 def test_single_numeral(single_numeral, expected):
     assert roman.Numeral(single_numeral) == expected
