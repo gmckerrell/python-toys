57799845192f5439862b826152b3905515e6e3aa
Graeme McKerrell <31652935+gmckerrell@users.noreply.github.com>
2020-06-01 12:54:16 +0100

    Update test_roman.py

--- romanNumerals/test_roman.py
+++ romanNumerals/test_roman.py
@@ -35,12 +35,13 @@ def test_single_numeral(single_numeral, expected):
 @pytest.mark.parametrize(
     "multiple_numerals, expected", 
     [
-        ('I',     1),
-        ('II',    2),
-        ('III',   3),
-        ('IV',    4),
-        ('V',     5),
-        ('IX',    9),
+        ('XI',           11),
+        ('XVI',          16),
+        ('XXVI',         26),
+        ('LXXXVI',       86),
+        ('CCLXXXVI',    286),
+        ('DCLXXXVI',    686),
+        ('MMDCLXXXVI', 2686)
     ]
 )
 
@@ -48,3 +49,54 @@ def test_single_numeral(single_numeral, expected):
 def test_multiple_numerals(multiple_numerals, expected):
     assert roman.Numeral(multiple_numerals) == expected
 
+
+@pytest.mark.parametrize(
+    "unrecognised_numeral, expected", 
+    [
+        ('xI',  'unrecognised numeral [xI]'),
+        ('IIx', 'unrecognised numeral II[x]'),
+        ('IiV', 'unrecognised numeral I[iV]')
+    ]
+)
+    
+
+def test_unrecognised_numeral(unrecognised_numeral, expected):
+    assert roman.Numeral(unrecognised_numeral) == expected
+
+    
+def test_repeating_digits():
+    assert roman.Numeral('MMMDCCIII') == 3703
+    
+
+@pytest.mark.parametrize(
+    "bad_repetition, expected", 
+    [
+        ('IIII',   'bad repetition III[I]'),
+        ('CVVII',  'bad repetition CV[V]II'),
+        ('CLLII',  'bad repetition CL[L]II'),
+        ('MDDII',  'bad repetition MD[D]II')
+    ]
+)
+
+
+def test_bad_repetition(bad_repetition, expected):
+    assert roman.Numeral(bad_repetition) == expected
+    
+
+@pytest.mark.parametrize(
+    "bad_increasing_value, expected", 
+    [
+        ('IC',   'increasing value I[C]'),
+        ('ID',   'increasing value I[D]'),
+        ('IM',   'increasing value I[M]'),
+        ('VX',   'increasing value V[X]'),
+        ('VL',   'increasing value V[L]'),
+        ('VCVI', 'increasing value V[C]VI'),
+        ('VD',   'increasing value V[D]'),
+        ('VMCC', 'increasing value V[M]CC'),
+        ('IL',   'increasing value I[L]')
+)
+
+     
+def test_bad_increasing_value(bad_increasing_value, expected):
+    assert roman.Numeral(bad_increasing_value) == expected
