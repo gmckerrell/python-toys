51a760c5780043e5c5bcd462fadaa02ece7ae210
McKerrell, Graeme <Graeme_McKerrell@McAfee.com>
2020-11-12 12:35:33 +0000

    start to tidy up object_oriented version

--- carhire/object_oriented/server/db.py
+++ carhire/object_oriented/server/db.py
@@ -61,63 +61,46 @@ class CarHire:
             c = self.cursor()
             c.executescript(
                 """
+                DROP TABLE IF EXISTS models;
                 CREATE TABLE models (
-                    id            INTEGER PRIMARY KEY,
-                    name          TEXT    NOT NULL,
-                    manufacturer  TEXT    NOT NULL,
-                    luggage       INTEGER NOT NULL,
-                    people        INTEGER NOT NULL
+                    id            INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name          TEXT    UNIQUE      NOT NULL,
+                    manufacturer  TEXT                NOT NULL,
+                    luggage       INTEGER             NOT NULL,
+                    people        INTEGER             NOT NULL
                 );
+
+                DROP TABLE IF EXISTS cars;
                 CREATE TABLE cars (
-                    id           INTEGER PRIMARY KEY,
-                    registration TEXT    NOT NULL,
-                    model_id     INTEGER NOT NULL,
-                    FOREIGN KEY (model_id)
-                        REFERENCES models (id)
-                            ON DELETE RESTRICT
+                    id           INTEGER PRIMARY KEY  AUTOINCREMENT,
+                    registration TEXT    UNIQUE       NOT NULL,
+                    color        TEXT                 NOT NULL,
+                    model_id     INTEGER              NOT NULL,
+                    
+                    FOREIGN KEY(model_id) REFERENCES models(id)
                 );
+
+                DROP TABLE IF EXISTS bookings;
                 CREATE TABLE bookings (
                     id         INTEGER PRIMARY KEY AUTOINCREMENT,
-                    customer   TEXT    NOT NULL,
-                    start_date DATE    NOT NULL,
-                    end_date   DATE    NOT NULL,
-                    car_id     INTEGER NOT NULL,
-                    FOREIGN KEY (car_id)
-                        REFERENCES cars (id)
-                            ON DELETE RESTRICT
+                    customer   TEXT                NOT NULL,
+                    start_date DATE                NOT NULL,
+                    end_date   DATE                NOT NULL,
+                    car_id     INTEGER             NOT NULL,
+                    
+                    FOREIGN KEY(car_id) REFERENCES cars(id)
                 );
                 """
-            )
-            c.executemany(
-                "INSERT INTO models VALUES(?,?,?,?,?)",
-                (
-                    (1, "s-max", "Ford", 4, 5),
-                    (2, "mini", "BMW",   2, 4),
-                )
-            )
-            c.executemany(
-                "INSERT INTO cars VALUES(?,?,?)",
-                (
-                    (1, "ABC 123", 1),
-                    (2, "ABC 456", 1),
-                    (3, "M1N1 1",  2),
-                    (4, "M1N1 2",  2),
-                )
-            )
-            c.executemany(
-                "INSERT INTO bookings (customer, start_date, end_date, car_id) VALUES(?,?,?,?)",
-                (
-                    ("Fred Flintstone", "11-09-2020", "12-09-2020", 3),
-                    ("Barney Rubble", "11-09-2020", "20-10-2020", 2),
-                    ("Betty Rubble", "11-09-2020", "20-10-2020", 1),
-                )
             )
 
     def find_models(self, **criteria):
         where_expression = WhereExpression(
             {
-                "name":         "models.name LIKE ?",
-                "manufacturer": "models.manufacturer LIKE ?"
+                "id":           "id           =    ?",
+                "name":         "name         LIKE ?",
+                "manufacturer": "manufacturer LIKE ?",
+                "people":       "people       =    ?",
+                "luggage":      "luggage      =    ?",
             },
             criteria
         )
@@ -127,8 +110,8 @@ class CarHire:
                 f"""
                 SELECT
                     models.id,
-                    models.manufacturer,
                     models.name,
+                    models.manufacturer,
                     models.people,
                     models.luggage
                 FROM models
@@ -137,6 +120,31 @@ class CarHire:
                 where_expression.values
             )
             return c.fetchall()
+        
+    def create_model(self, name, manufacturer, people, luggage):
+        print(f"LOCALS: {locals()}")
+        with conn:
+            c = self.cursor()
+            try:
+                c.execute(
+                    """
+                    INSERT INTO models (name, manufacturer, people, luggage) VALUES(?,?,?,?)
+                    """,
+                    (name, manufacturer, people, luggage)
+                )
+            except sqlite3.IntegrityError as err:
+                raise ValueError(*err.args)
+
+    def delete_model(self, id):
+        with conn:
+            c = self.cursor()
+            c.execute(
+                """
+                DELETE FROM models WHERE id = ?
+                """,
+                (id,)
+            )
+            
 
     def find_cars(self, **criteria):
         where_expression = WhereExpression(
@@ -207,12 +215,24 @@ if __name__ == "__main__":
 
     dbpath = "carhire.db"
     first_time = not os.path.exists(dbpath)
-    conn = sqlite3.connnect(dbpath)
+    conn = sqlite3.connect(dbpath)
 
     car_hire = CarHire(conn)
 
-    if first_time:
-        car_hire.initialise()
+    car_hire.initialise()
+
+    car_hire.create_model(name = "s-max", manufacturer="Ford", people=5, luggage=5)
+    car_hire.create_model(name = "mini", manufacturer="BMW", people=4, luggage=2)
+
+#    create_car(conn, model="s-max", registration="ABC 123", color="red")
+#    create_car(conn, model="s-max", registration="ABC 456", color="blue")
+#    create_car(conn, model="mini",  registration="M1N1 2",  color="black")
+#    create_car(conn, model="mini",  registration="M1N1 1",  color="silver")
+
+#    create_booking(conn, start_date="11-09-2020", end_date="12-09-2020", customer="Fred Flintstone", model="mini")
+#    create_booking(conn, start_date="13-09-2020", end_date="15-09-2020", customer="Wilma Flintstone", model="mini")
+#    create_booking(conn, start_date="11-09-2020", end_date="12-09-2020", customer="Barney Rubble", model="s-max")
+#    create_booking(conn, start_date="13-09-2020", end_date="15-09-2020", customer="Betty Rubble", model="s-max")
 
     print("ALL MODELS")
     for model in car_hire.find_models():
